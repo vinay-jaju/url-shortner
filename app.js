021fb72d07142c5c36dc4f3683b3f01ef692ea07
Jaju <vjaju@deloitte.com>
2019-11-30 17:49:26 +0530

    Node js express basics complete

---
+++ app.js
@@ -0,0 +1,26 @@
+const path=require('path');
+
+const express=require('express');
+
+const rootDir=require('./util/path');
+
+const  bodyParser=require('body-parser');
+const app=express();
+
+const adminRoutes=require("./routes/admin");
+const shopRoutes=require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(rootDir,'public')));
+
+//routes  below
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+
+app.use((req,res,next)=>{
+    
+    res.status(404).sendFile(path.join(rootDir,'views','404.html'));
+});
+
+
+app.listen(3000);
---
+++ routes/admin.js
@@ -0,0 +1,21 @@
+const path=require('path');
+const express=require('express');
+
+const router=express.Router();
+
+router.get('/add-product',(req, res, next)=>{
+    console.log('In another middleware');
+    res.sendFile(path.join(__dirname,'../','views','add-product.html'));
+   
+
+});
+
+
+router.post('/add-product',(req, res, next)=>{
+    console.log(req.body);
+    res.redirect('/')
+   
+
+});
+
+module.exports=router;
