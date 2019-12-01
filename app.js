165d9a8d45db71decc5ec2cf53f0e0e0f3b06b44
Jaju <vjaju@deloitte.com>
2019-12-01 12:17:03 +0530

    URL shortner with Mongo

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
-const path=require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const mongoURI = "mongodb://localhost:27017/url-shortner";
+const connectOptions = { 
+  keepAlive: true, 
+  reconnectTries: Number.MAX_VALUE 
+}; 
+//Connect to MongoDB 
+mongoose.Promise = global.Promise; 
+mongoose.connect(mongoURI, connectOptions, (err, db) => 
+{ 
+  if (err) console.log(`Error`, err); 
+  console.log(`Connected to MongoDB`); 
+}); 
+require('./models/UrlShorten');
+const app = express(); 
+app.use(bodyParser.json());
+const PORT = 7000; 
+app.use(require("./routes/urlshorten"));
 
-const express=require('express');
-
-const rootDir=require('./util/path');
-
-const  bodyParser=require('body-parser');
-const app=express();
-
-const adminRoutes=require("./routes/admin");
-const shopRoutes=require('./routes/shop');
-
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(express.static(path.join(rootDir,'public')));
-
-//routes  below
-app.use('/admin',adminRoutes);
-app.use(shopRoutes);
-
-app.use((req,res,next)=>{
-    
-    res.status(404).sendFile(path.join(rootDir,'views','404.html'));
-});
-
-
-app.listen(3000);
+app.listen(PORT);
